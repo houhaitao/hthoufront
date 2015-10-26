68faea549c903283530c312d499c3d01b908f495
无聊小刚 <haitao_hou@126.com>
2015-10-26 15:41:06 +0800

    删除所有

--- dist/js/fuckie6.js
+++
@@ -1,6 +0,0 @@
-//author menghao
-if(!window.XMLHttpRequest){
-    document.write('<style type="text/css">#ie6-warning{ background:rgb(255,255,225); position:absolute; top:0; left:0; right:0; font-size:12px; color:#333; padding: 2px 0 2px 23px; text-align:left; } #ie6-warning a { text-decoration:none; }</style>');
-    document.write('<div id="ie6-warning">您正在使用 Internet Explorer 6，部分页面可能显示不正常。建议您升级到 <a href="http://www.microsoft.com/china/windows/internet-explorer/" target="_blank">Internet Explorer 8</a></div>');
-    document.getElementById("ie6-warning").style.width = document.body.clientWidth-23+'px';
-}
